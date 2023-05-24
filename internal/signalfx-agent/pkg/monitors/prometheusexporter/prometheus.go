fb4366429faca8ee85c68f20f6df38b03da4d810
Antoine Toulme <atoulme@splunk.com>
2023-05-24 00:21:06 -0700

    Migrate the prometheus monitor to use pmetric data directly

--- internal/signalfx-agent/pkg/monitors/prometheusexporter/prometheus.go
+++ internal/signalfx-agent/pkg/monitors/prometheusexporter/prometheus.go
@@ -12,8 +12,8 @@ import (
 
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
-	"github.com/signalfx/golib/v3/datapoint"
 	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 	"k8s.io/client-go/rest"
 
 	"github.com/signalfx/signalfx-agent/pkg/core/common/auth"
@@ -134,29 +134,30 @@ func (m *Monitor) Configure(conf *Config) error {
 	var ctx context.Context
 	ctx, m.cancel = context.WithCancel(context.Background())
 	utils.RunOnInterval(ctx, func() {
-		dps, err := fetchPrometheusMetrics(fetch)
+		metrics, err := fetchPrometheusMetrics(fetch)
 		if err != nil {
 			m.logger.WithError(err).Error("Could not get prometheus metrics")
 			return
 		}
 
-		m.Output.SendDatapoints(dps...)
+		m.Output.SendMetrics(metrics)
 	}, time.Duration(conf.IntervalSeconds)*time.Second)
 
 	return nil
 }
 
-func fetchPrometheusMetrics(fetch fetcher) ([]*datapoint.Datapoint, error) {
+func fetchPrometheusMetrics(fetch fetcher) (pmetric.Metrics, error) {
+	metrics := pmetric.NewMetrics()
 	metricFamilies, err := doFetch(fetch)
 	if err != nil {
-		return nil, err
+		return metrics, err
 	}
+	sm := metrics.ResourceMetrics().AppendEmpty().ScopeMetrics().AppendEmpty()
 
-	var dps []*datapoint.Datapoint
 	for i := range metricFamilies {
-		dps = append(dps, convertMetricFamily(metricFamilies[i])...)
+		convertMetricFamily(sm, metricFamilies[i])
 	}
-	return dps, nil
+	return metrics, nil
 }
 
 func doFetch(fetch fetcher) ([]*dto.MetricFamily, error) {
